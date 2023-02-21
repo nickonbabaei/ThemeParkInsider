f20c1bb30c4588b5467aac5f69b6ab850a2e5afb
Hatice Yorukoglu <yorukogluhatice@gmail.com>
2023-02-21 15:58:38 -0500

    modified review key

--- models/themePark.js
+++ models/themePark.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const ThemePark = new Schema(
-    {
-        name: { type: String, required: true },
-        location: { type: String, required: true },
-        description: { type: String, required: true },
-        price: { type: String, required: false },
-        image: { type: String, required: true },
-        review: { type: [Schema.Types.ObjectId], ref: 'Review', required: true },
-    },
-    { timestamps: true },
+  {
+    name: { type: String, required: true },
+    location: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: String, required: false },
+    image: { type: String, required: true },
+    review: [{ type: Schema.Types.ObjectId, ref: 'Review', required: true }]
+  },
+  { timestamps: true }
 )
 
 module.exports = mongoose.model('ThemePark', ThemePark)
