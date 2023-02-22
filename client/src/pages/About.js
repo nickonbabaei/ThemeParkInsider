902fec4da84529bba1453f820d3db735c798f3e2
Hatice Yorukoglu <yorukogluhatice@gmail.com>
2023-02-22 11:19:38 -0500

    About page is ready.

---
+++ client/src/pages/About.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div className="body">
+      <p className="about">
+        Theme Park Insider is a website that includes the basic information and
+        reviews about the theme parks in the USA. Anyone can add reviews about
+        the theme parks and rates them overall.
+      </p>
+    </div>
+  )
+}
+
+export default About
