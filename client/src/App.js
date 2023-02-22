38df320a38835c971e316d423ef5f8b64be23a04
NellyNel520 <channelharris520@gmail.com>
2023-02-22 12:24:44 -0500

    corrected nav bar, corrected index.js browser router import, added nav to app.js and created routes for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+// import { Router } from 'express';
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
+import Home from './pages/Home'
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+
+
+	return (
+		<div className="App">
+    <header>
+      <Nav />
+    </header>
+			<main>
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</main>
+		</div>
+	);
 }
 
-export default App;
+export default App
