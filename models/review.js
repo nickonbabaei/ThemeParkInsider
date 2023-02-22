59e0f957ea100fcb37583af43b267921bd8689b7
NellyNel520 <channelharris520@gmail.com>
2023-02-21 23:37:25 -0500

    changed review schema to reference theme park object id so we can add specfic reciews to certain parks and push the object id for that review into the reviews array in the theme park model

--- models/review.js
+++ models/review.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 const Review = new Schema(
     {
         comment: { type: String, required: true },
-        rating: { type: String, required: false }
+        rating: { type: String, required: false },
+        themePark: { type: Schema.Types.ObjectId, ref: 'ThemePark'}
     },
     { timestamps: true },
 )
