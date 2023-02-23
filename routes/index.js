16f0b7e0b327cd0fb24407d69429acf13d67f06f
Nickon Babaei <babaein1@gmail.com>
2023-02-23 13:58:18 -0300

    cleaned code

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,18 @@
 const { Router } = require('express');
 const router = Router();
 const controllers = require('../controllers')
-// Test (passed)
-router.get('/', (req, res) => res.send('This is root howdy hey !'))
 
+// Create Review (passed)
 router.post('/themePark/:id/review', controllers.createReview)
 // Create ThemePark (passed)
 router.post('/themePark', controllers.createThemePark)
-// Create Review (passed)
-
-
 // Get All Parks (passed)
 router.get('/themeParks', controllers.getAllParks)
-
 // Get parks by id (passed)
 router.get('/themeParks/:id', controllers.getParkById)
 // Get Reviews by ID (passed insomnia test)
 router.get('/reviews/:id', controllers.getReviewById)
-
-
 // delete review***
 router.delete('/deleteReview/:id', controllers.deleteReview)
-
-
-
-
-
 
 module.exports = router;
